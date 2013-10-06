3e3056deb110344b826b43db800d3b480bf8f932
lhuelite2012 <lhuelite2012@gmail.com>
2013-10-06 20:36:13 +0800

    test

--- testSession.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$test = 123;
-	session_register($test);
-	header("location:testSession2.php");
-?>
--- testSession2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-
-	echo $test;
-
-	echo "NO";
-
-?>

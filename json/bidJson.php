5026d05a06337310ba751d07be8779b4b01e2728
Guan <chocolateegg55@gmail.com>
2013-10-16 01:21:35 +0800

    push

--- json/bidJson.php
+++ json/bidJson.php
@@ -1,12 +1,12 @@
-<?php
-	include("../phpFunction.php");
+<? include("../phpFunction.php");
+	
 	//沒有判斷 帳號登入
 	//出價等於最高價時 直接結束
 	//出價低於最高價時 直接結束
 	$json = array();
 	include("../server.php");
 	include("../addtime.php");    //系統時間
-	
+
 	$c_number = $_REQUEST['c_number'];
 	$bid = $_REQUEST['bid'];
 	$m_number = $_REQUEST['m_number'];
@@ -26,7 +26,7 @@
 		include("pushJson.php");
 		
 		//出價最高
-		$bid_hi_sql = "select max(bid_price) from bid where c_number = '$c_number' group by c_number limit 1";
+		$bid_hi_sql = "select max(bid_price) from bid where c_number = $c_number limit 1";
 		$bid_hi_query = mysql_query($bid_hi_sql);
 		$bid_hi_rows = mysql_fetch_row($bid_hi_query);
 		$bid_commodity_sql = "update commodity set hi_bid_price = ".$bid_hi_rows[0]." where c_number = $c_number";
@@ -37,7 +37,8 @@
 		else
 			$json['yy']=3;   //已是目前最高出價者
 	}else
-		$json['yy']=2; //未大於最高
+		$json['yy']=2;
 		
 	echo json_encode($json);
 ?>
+</body></html>
